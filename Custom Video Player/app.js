9ed740b7027c30e52a7ffc4266688620f0915988
Devisetti Teja <71646126+Teja54@users.noreply.github.com>
2021-05-22 15:55:37 +0530

    Add files via upload

---
+++ Custom Video Player/app.js
@@ -0,0 +1,96 @@
+/* Get Our Elements */
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+const current = document.querySelector(".current");
+const duration = document.querySelector(".duration");
+const fullScreen = player.querySelector(".fullscreenBtn");
+
+// Create fullscreen video button
+function toggleFullscreen() {
+  if (player.requestFullScreen) {
+    player.requestFullScreen();
+  } else if (player.webkitRequestFullScreen) {
+    player.webkitRequestFullScreen();
+  } else if (player.mozRequestFullScreen) {
+    player.mozRequestFullScreen();
+  }
+}
+
+/* Build out functions */
+function togglePlay() {
+  const method = video.paused ? "play" : "pause";
+  video[method]();
+}
+
+function updateButton() {
+  const icon = this.paused ? "►" : "❚ ❚";
+  toggle.textContent = icon;
+}
+
+function skip() {
+  video.currentTime += parseFloat(this.dataset.skip);
+}
+
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = (video.currentTime / video.duration) * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
+// Get currnet time of video and append
+function currentTime() {
+  var curmins = Math.floor(video.currentTime / 60);
+  var cursecs = Math.floor(video.currentTime - curmins * 60);
+  var durmins = Math.floor(video.duration / 60);
+  var dursecs = Math.floor(video.duration - durmins * 60);
+  if (cursecs < 10) {
+    cursecs = "0" + cursecs;
+  }
+  if (dursecs < 10) {
+    dursecs = "0" + dursecs;
+  }
+  if (curmins < 10) {
+    curmins = "0" + curmins;
+  }
+  if (durmins < 10) {
+    durmins = "0" + durmins;
+  }
+  current.innerHTML = curmins + ":" + cursecs;
+  duration.innerHTML = durmins + ":" + dursecs;
+  console.log(Math.floor(video.currentTime));
+}
+
+/* Hook up the event listeners */
+video.addEventListener("click", togglePlay);
+video.addEventListener("play", updateButton);
+video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", currentTime);
+video.addEventListener("timeupdate", handleProgress);
+
+toggle.addEventListener("click", togglePlay);
+fullScreen.addEventListener("click", toggleFullscreen);
+
+skipButtons.forEach((button) => button.addEventListener("click", skip));
+ranges.forEach((range) => range.addEventListener("change", handleRangeUpdate));
+ranges.forEach((range) =>
+  range.addEventListener("mousemove", handleRangeUpdate)
+);
+
+let mousedown = false;
+progress.addEventListener("click", scrub);
+progress.addEventListener("mousemove", (e) => mousedown && scrub(e));
+progress.addEventListener("mousedown", () => (mousedown = true));
+progress.addEventListener("mouseup", () => (mousedown = false));
